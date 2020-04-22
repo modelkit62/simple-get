74348047b88c63e683184052b11dad84c886c3ed
d r <modelkit62@gmail.com>
2020-04-22 16:53:11 +0100

    first commit

---
+++ src/test/java/com/example/simpleget/SimpleGetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
